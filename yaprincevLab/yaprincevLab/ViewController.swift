6bd1aa01a0fca4fcc836882cb59fd3278fd20600
yaprincev <5489812@mail.ru>
2023-02-03 19:14:05 +0300

    Create all files

---
+++ yaprincevLab/yaprincevLab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yaprincevLab
+//
+//  Created by Максим Япринцев on 03.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
